0b15f1537e9728d0b306d1c421f07038350853aa
Fatmasarhan <fatmasar7an@gmail.com>
2016-05-21 18:02:34 +0200

    Adding orders page

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -69,6 +69,6 @@ class OrdersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def order_params
-      params.require(:order).permit(:type, :restaurant, :menu_image, :user_id)
+      params.require(:order).permit(:type, :restaurant, :menu_image, :user_id ,:status)
     end
 end
